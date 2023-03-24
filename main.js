6d62157938821466e442e2bffd81d159128f8def
paulocpavan <paulo.c.pavan@gmail.com>
2023-03-24 20:01:18 -0300

    parar audio qdo comeca novo

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).pause()
-    document.querySelector(idElementoAudio).play();
+let audioAtual;
+
+function tocaSom(idElementoAudio) {
+  // Pausa o áudio anterior, se existir
+  if (audioAtual) {
+    audioAtual.pause();
+  }
+
+  const audioElemento = document.querySelector(idElementoAudio);
+  audioElemento.play();
+
+  // Atualiza a referência para o áudio em reprodução
+  audioAtual = audioElemento;
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
-
-    tecla.onclick = function () {
-        
-        tocaSom(idAudio);
-    }
-
+  tecla.onclick = function() {
+    tocaSom(idAudio);
+  };
 }
